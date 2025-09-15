d5ad273f6953cd8b57d231b552103eb922e4fe30
nanookim <inggit.prasetyooo@gmail.com>
2025-09-15 23:50:57 +0700

    fix (API) : reset password

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,8 @@ Route::post('/kpsp/{id_set}', [KpspUserController::class, 'store'])->name('kpsp.
 Route::get('forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::post('forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
 
-Route::get('reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('reset-password', [ResetPasswordController::class, 'reset'])->name('password.update');
+Route::get('reset-password/{token}', [\App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('reset-password', [\App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
 
 
 
